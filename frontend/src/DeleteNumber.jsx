8fb7f7ab75b3810e2880f559dd15d7d324fc7bbb
Bismark Kadogbe <neroaboagye@gmail.com>
2023-03-26 20:26:57 +0000

    *

--- frontend/src/DeleteNumber.jsx
+++ frontend/src/DeleteNumber.jsx
@@ -1,21 +1,28 @@
 import React, { useState } from "react";
 import Axios from "axios";
 // import { useNavigate } from "react-router";
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams, useNavigate, Link } from "react-router-dom";
 
-const DeleteNumber = () => {
+const DeleteNumber = (props) => {
   const [newPhone, setNewPhone] = useState([]);
   const navigate = useNavigate();
   const { id } = useParams();
 
   const deletePhone = () => {
-    Axios.delete(`http://localhost:8080/get-phone/${id}`)
+    Axios.delete(`http://localhost:8080/get-phone/${props.id}`)
       .then((res) => {
         console.log(res.data);
+        console.log("Deleted");
       })
       .catch((err) => console.log(err));
   };
-  return deletePhone();
+  return (
+    <div>
+      <button onClick={deletePhone} id={props.id}>
+        {props.name}
+      </button>
+    </div>
+  );
 };
 
 export default DeleteNumber;
--- frontend/src/EditNumber.jsx
+++ frontend/src/EditNumber.jsx
@@ -3,39 +3,6 @@ import Axios from "axios";
 import { useParams } from "react-router-dom";
 
 const EditNumber = () => {
-  // const [newPhone, setNewPhone] = useState([]);
-  //  const [phonebook, setPhonebook] = useState([]);
-  // const updatePhone = (id) => {
-  //   Axios.put("http://localhost:5000/update-phone", { id, newPhone });
-  // };
-  // return (
-  //   <div className="container">
-  //     {newPhone.map((val, key) => {
-  //       return (
-  //         <div key={key} className="phone">
-  //           <h1>{val.name}</h1>
-  //           <h1>{val.phone}</h1>
-  //           <input
-  //             type="number"
-  //             placeholder="update Phone..."
-  //             onChange={(e) => {
-  //               setNewPhone(e.target.value);
-  //             }}
-  //           />
-  //           <button
-  //             className="update-btn"
-  //             onClick={() => {
-  //               updatePhone(val._id);
-  //             }}
-  //           >
-  //             Update
-  //           </button>
-  //         </div>
-  //       );
-  //     })}
-  //   </div>
-  // );
-
   /**
    * First set useState to hold the fetch data
    */
@@ -48,21 +15,13 @@ const EditNumber = () => {
    * Update data to the backend function when the form is submitted
    */
 
-  Axios.put(`http://localhost:5000/update-phone/${id}`, formData)
+  Axios.patch(`http://localhost:5000/api/get-phone/${id}`, formData)
     .then((res) => {
       console.log(res.data);
     })
     .catch((err) => console.log(err));
 
   const updateData = () => {
-    // Axios.get(`http://localhost:5000/get-phone/${id}`)
-    //   .then((res) => {
-    //     console.log(res.data.data.phoneNumbers._id);
-    //     setName(res.data.data.phoneNumbers);
-    //     setPhone(res.data.data.phoneNumbers);
-    //   })
-    //   .catch((err) => console.log(err));
-
     Axios.patch(`http://localhost:5000/update-phone/${id}`, formData)
       .then((res) => {
         console.log(res.data);
