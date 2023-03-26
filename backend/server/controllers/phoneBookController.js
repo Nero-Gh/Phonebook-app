560a22f5c366cccaa1ccf9a98f1cf1a341597441
Bismark Kadogbe <neroaboagye@gmail.com>
2023-03-26 20:14:25 +0000

    Updated api endpoint

--- backend/server/controllers/phoneBookController.js
+++ backend/server/controllers/phoneBookController.js
@@ -46,61 +46,27 @@ exports.getContact = async (req, res) => {
 };
 
 /**
- * Getting data from the database by id
+ * Updating data from the database by id
  */
 
-exports.getContactById = async (req, res) => {
-  const phoneNumber = await phoneBook.findById(req.params.id);
-
+exports.updateContactById = async (req, res) => {
+  const updatedPhone = await phoneBook.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
   try {
     res.status(200).json({
       status: "Success",
       data: {
-        phoneNumber,
+        updatedPhone,
       },
     });
   } catch (err) {
-    res.status(500).json({
-      status: "Failed",
-      message: err,
-    });
-  }
-};
-
-/**
- * Updating data from the database by id
- */
-
-// exports.updateContactById = async (req, res) => {
-//   const updatedPhone = await phoneBook.findByIdAndUpdate(
-//     req.params.id,
-//     req.body,
-//     {
-//       new: true,
-//       runValidators: true,
-//     }
-//   );
-//   try {
-//     res.status(200).json({
-//       status: "Success",
-//       data: {
-//         updatedPhone,
-//       },
-//     });
-//   } catch (err) {
-//     console.log(err);
-//   }
-// };
-
-exports.updateContactById = async (req, res) => {
-  try {
-    const updatedPhone = await phoneBook.findByIdAndUpdate(
-      req.params.id,
-      req.body
-    );
-    res.send(updatedPhone);
-  } catch (error) {
-    res.status(500).send(error);
+    console.log(err);
   }
 };
 
--- backend/server/routes/phoneBook.js
+++ backend/server/routes/phoneBook.js
@@ -7,18 +7,15 @@ const phoneBookController = require("../controllers/phoneBookController");
  */
 
 // Adding post route to be ble to add new contact
-router.post("/add-phone", phoneBookController.addContact);
+router.post("/api/add-phone", phoneBookController.addContact);
 
 //Geting data from the database
-router.get("/get-phone", phoneBookController.getContact);
-
-//Geting data from the database by id
-router.get("/get-phone/:id", phoneBookController.getContactById);
+router.get("/api/get-phone", phoneBookController.getContact);
 
 //Updating data from the database by id
-router.put("/update-phone/:id", phoneBookController.updateContactById);
+router.put("/api/get-phone/:id", phoneBookController.updateContactById);
 
 //Deleting data from the database by id
-router.delete("/delete-phone/:id", phoneBookController.deleteContact);
+router.delete("/api/delete-phone/:id", phoneBookController.deleteContact);
 
 module.exports = router;
